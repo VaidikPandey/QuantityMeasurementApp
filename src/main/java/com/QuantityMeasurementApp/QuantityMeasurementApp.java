dc92c4b9400d681ab94284ba3f8e68ac36eef459
Vaidik Pandey <pandeyvaidik04@gmail.com>
2026-02-26 14:43:47 +0530

    [Vaidik Pandey] . made changes in the main function of UC4.

--- src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -2,13 +2,18 @@ package com.QuantityMeasurementApp;
 
 public class QuantityMeasurementApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+	    QuantityLength yard = new QuantityLength(1.0, LengthUnit.YARDS);
+	    QuantityLength feet = new QuantityLength(3.0, LengthUnit.FEET);
+	    QuantityLength inches = new QuantityLength(36.0, LengthUnit.INCH);
+	    QuantityLength cm = new QuantityLength(2.54, LengthUnit.CENTIMETER);
+	    QuantityLength inch = new QuantityLength(1.0, LengthUnit.INCH);
 
-        System.out.println("Equal? " + q1.equals(q2));
-    }
+	    System.out.println("1 yard equals 3 feet? " + yard.equals(feet));
+	    System.out.println("1 yard equals 36 inches? " + yard.equals(inches));
+	    System.out.println("2.54 cm equals 1 inch? " + cm.equals(inch));
+	}
 }
 
 
