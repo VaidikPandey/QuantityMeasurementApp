c45293073405b36f3cba75b64c1ffb28aad5879c
Vaidik Pandey <pandeyvaidik04@gmail.com>
2026-02-26 14:47:21 +0530

    [Vaidik Pandey] . made changes in the main function of UC5.

--- src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -2,13 +2,20 @@ package com.QuantityMeasurementApp;
 
 public class QuantityMeasurementApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+	    System.out.println("1 foot in inches: " +
+	            QuantityLength.convert(1.0, LengthUnit.FEET, LengthUnit.INCH));
 
-        System.out.println("Equal? " + q1.equals(q2));
-    }
+	    System.out.println("3 yards in feet: " +
+	            QuantityLength.convert(3.0, LengthUnit.YARDS, LengthUnit.FEET));
+
+	    System.out.println("36 inches in yards: " +
+	            QuantityLength.convert(36.0, LengthUnit.INCH, LengthUnit.YARDS));
+
+	    System.out.println("2.54 cm in inches: " +
+	            QuantityLength.convert(2.54, LengthUnit.CENTIMETER, LengthUnit.INCH));
+	}
 }
 
 
