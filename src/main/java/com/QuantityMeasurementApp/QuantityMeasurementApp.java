225d22661972e68b0712c82a44c96c9eecb9af93
Vaidik Pandey <pandeyvaidik04@gmail.com>
2026-02-26 14:51:40 +0530

    [Vaidik Pandey] . made changes in the main function of UC7.

--- src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -2,13 +2,31 @@ package com.QuantityMeasurementApp;
 
 public class QuantityMeasurementApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+	    QuantityLength a = new QuantityLength(1.0, LengthUnit.FEET);
+	    QuantityLength b = new QuantityLength(12.0, LengthUnit.INCH);
 
-        System.out.println("Equal? " + q1.equals(q2));
-    }
+	    QuantityLength sumFeet =
+	            QuantityLength.add(a, b, LengthUnit.FEET);
+	    System.out.println("Sum in feet: " + sumFeet);
+
+	    QuantityLength sumInches =
+	            QuantityLength.add(a, b, LengthUnit.INCH);
+	    System.out.println("Sum in inches: " + sumInches);
+
+	    QuantityLength sumYards =
+	            QuantityLength.add(a, b, LengthUnit.YARDS);
+	    System.out.println("Sum in yards: " + sumYards);
+
+	    QuantityLength sumCm =
+	            QuantityLength.add(
+	                    new QuantityLength(2.54, LengthUnit.CENTIMETER),
+	                    new QuantityLength(1.0, LengthUnit.INCH),
+	                    LengthUnit.CENTIMETER);
+
+	    System.out.println("2.54 cm + 1 in in cm = " + sumCm);
+	}
 }
 
 
