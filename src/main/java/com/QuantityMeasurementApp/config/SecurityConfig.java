edb53c7bc3400546bdc67c197109674a7eab8e04
Vaidik Pandey <pandeyvaidik04@gmail.com>
2026-04-12 17:05:16 +0530

    feat: add Swagger UI with JWT auth support.

--- src/main/java/com/QuantityMeasurementApp/config/SecurityConfig.java
+++ src/main/java/com/QuantityMeasurementApp/config/SecurityConfig.java
@@ -41,11 +41,17 @@ public class SecurityConfig {
             )
 
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login/**").permitAll()
-                .requestMatchers("/oauth2/**").permitAll()
-                .requestMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated()
-            )
+            	    .requestMatchers("/login/**").permitAll()
+            	    .requestMatchers("/oauth2/**").permitAll()
+            	    .requestMatchers("/auth/**").permitAll()
+            	    .requestMatchers(
+            	        "/swagger-ui/**",
+            	        "/swagger-ui.html",
+            	        "/v3/api-docs/**",
+            	        "/v3/api-docs"
+            	    ).permitAll()
+            	    .anyRequest().authenticated()
+            	)
 
             .oauth2Login(oauth2 -> oauth2
                 .userInfoEndpoint(userInfo -> userInfo
